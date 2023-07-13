2968aeb86afafead1d3eccaa6bcffe5469316a29
Egor <lldocll2012@outlook.com>
2023-07-13 21:43:18 +0300

    2 задача

--- home_work_task2/Program.cs
+++ home_work_task2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Введите первое число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{
+    Console.Write("Большее число= "); 
+    Console.Write(b);
+}
+ if (b < a)
+ {
+    Console.Write("Большее число= "); 
+    Console.Write(a);
+ }
+
+ if (a < c)
+{
+    Console.Write("Большее число= "); 
+    Console.Write(c);
+}
+
+if (b < c)
+{
+    Console.Write("Большее число= "); 
+    Console.Write(c);
+}
